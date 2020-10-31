5011927a43c6b070db47fc36e720a54d1a7da3c5
joeldevel <christianjoel.developer@gmail.com>
2020-10-31 11:09:37 -0300

    v1 done

--- js/cotizador.js
+++ js/cotizador.js
@@ -3,23 +3,13 @@ const COSTO = {
   dosDias: 45,
   fullDias: 50
 }
-// const COSTO_UN_DIA = 30;
-// const COSTO_DOS_DIAS = 45;
-// const COSTO_FULL_DIAS = 50;
-
 
 btnCalcular = document.querySelector('#calcular');
 
-function mostrarCantidad() {
-  const cantidadUnDia = document.querySelector('#pase_dia').value;
-  const cantidadDosDias = document.querySelector('#pase_dosdias').value;
-  const cantidadFullDias = document.querySelector('#pase_completo').value;
-  console.log(`${cantidadUnDia}, ${cantidadDosDias}, ${cantidadFullDias}`);
-}
 function calcularCosto() {
-  const cantidadUnDia = parseFloat(document.querySelector('#pase_dia').value);
-  const cantidadDosDias = parseFloat(document.querySelector('#pase_dosdias').value);
-  const cantidadFullDias = parseFloat(document.querySelector('#pase_completo').value);
+  const cantidadUnDia = parseInt(document.querySelector('#pase_dia').value);
+  const cantidadDosDias = parseInt(document.querySelector('#pase_dosdias').value);
+  const cantidadFullDias = parseInt(document.querySelector('#pase_completo').value);
   let total = cantidadUnDia* COSTO.unDia + cantidadDosDias * COSTO.dosDias + cantidadFullDias * COSTO.fullDias;
 
   return {
@@ -27,30 +17,35 @@ function calcularCosto() {
     total: total
   };
 }
+
 function detalleCosto(cant, costo, detalle) {
   return `${cant} * $${costo} ${detalle} = $${cant*costo}`;
 }
+
 function mostrarCosto(datos) {
-  console.log(datos.cantidades[0]);
-  let lista = document.createElement('ul');
+
+  let listaBoletos = document.createElement('ul');
+
   let li = document.createElement('li');
   let li2 = document.createElement('li');
   let li3 = document.createElement('li');
-  li.innerHTML = datos.cantidades[0]?detalleCosto(datos.cantidades[0], COSTO.unDia,'pase/s 1 dia/s'):'';
-  li2.innerHTML = datos.cantidades[1]?detalleCosto(datos.cantidades[1], COSTO.dosDias,'pase/s 2 dia/s'):'';
+
+  li.innerHTML = datos.cantidades[0]?detalleCosto(datos.cantidades[0], COSTO.unDia,'pase/s 1 día/s'):'';
+  li2.innerHTML = datos.cantidades[1]?detalleCosto(datos.cantidades[1], COSTO.dosDias,'pase/s 2 día/s'):'';
   li3.innerHTML = datos.cantidades[2]?detalleCosto(datos.cantidades[2], COSTO.fullDias,'pase/s completo/s'):'';
-  // let lis = datos[0].foreach((e)=>{`<li>${e}</li>`});
-  lista.appendChild(li);
-  lista.appendChild(li2);
-  lista.appendChild(li3);
+
+  listaBoletos.appendChild(li);
+  listaBoletos.appendChild(li2);
+  listaBoletos.appendChild(li3);
+
   document.querySelector('#lista-productos').innerHTML = '';
+
   if(li.innerHTML=='' && li2.innerHTML=='' && li3.innerHTML=='') {
-    document.querySelector('#lista-productos').innerHTML = "Ningun boleto seleccionado";
+    document.querySelector('#lista-productos').innerHTML = "Ningún boleto seleccionado";
   }
   else {
-    document.querySelector('#lista-productos').appendChild(lista);
+    document.querySelector('#lista-productos').appendChild(listaBoletos);
     document.querySelector('#suma-total').innerHTML = datos.total;
-
   }
 
 }
